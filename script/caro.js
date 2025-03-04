6ecdd40751729fda8a156baffecda976917f5c10
Claudio Galiano <claudiogal92@gmail.com>
2025-03-04 16:09:44 +0100

    fix vari

--- script/caro.js
+++ script/caro.js
@@ -22,25 +22,45 @@ for (let i = 0; i < totalSlides; i++) {
 */
 
 document.addEventListener("DOMContentLoaded", function () {
+  // Ripulisci il session storage al caricamento della pagina
+  sessionStorage.clear();
+
   const elements = document.querySelectorAll(".hidden");
 
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-        if (entry.target.classList.contains("count-up")) {
-          animateCountUp(entry.target);
+        const id = entry.target.id;
+
+        // Verifica se l'animazione è già stata eseguita
+        if (!sessionStorage.getItem(`animazioneEseguita-${id}`)) {
+          entry.target.classList.add("show");
+          entry.target.classList.remove("hidden");
+          sessionStorage.setItem(`animazioneEseguita-${id}`, "true");
+
+          if (entry.target.classList.contains("count-up")) {
+            animateCountUp(entry.target);
+          }
         }
       }
     });
   });
 
-  elements.forEach((el) => observer.observe(el));
+  // Aggiorna la visualizzazione iniziale degli elementi
+  elements.forEach((el) => {
+    const id = el.id;
+    if (sessionStorage.getItem(`animazioneEseguita-${id}`)) {
+      el.classList.add("show");
+      el.classList.remove("hidden");
+    } else {
+      observer.observe(el);
+    }
+  });
 
   function animateCountUp(element) {
     let start = 0;
     const end = parseInt(element.getAttribute("data-end"));
-    const duration = 2000;
+    const duration = 8000;
     const increment = end / (duration / 60);
 
     function updateCount() {
