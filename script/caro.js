33faff0ba1993f89efbccef062d6f0f56ba73588
Claudio Galiano <claudiogal92@gmail.com>
2025-03-05 13:20:19 +0100

    new section and fix

--- script/caro.js
+++ script/caro.js
@@ -1,25 +1,6 @@
-/*
-let currentSlide = 0;
-const slide = document.getElementById("carouselSlide");
-const items = document.querySelectorAll(".carousel-item");
-const totalSlides = items.length;
-const slideWidth = items[0].offsetWidth + 10; // 10px gap
+const currentYear = new Date().getFullYear();
 
-function moveSlide(direction) {
-  currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
-  slide.style.transform = `translateX(${-currentSlide * slideWidth}px)`;
-}
-
-function autoSlide() {
-  moveSlide(1);
-  setTimeout(autoSlide, 3000); // Cambia immagine ogni 3 secondi
-}
-
-// Raddoppia le immagini per evitare spazi bianchi alla fine
-for (let i = 0; i < totalSlides; i++) {
-  slide.appendChild(items[i].cloneNode(true));
-}
-*/
+document.getElementById("year").textContent = currentYear;
 
 document.addEventListener("DOMContentLoaded", function () {
   // Ripulisci il session storage al caricamento della pagina
